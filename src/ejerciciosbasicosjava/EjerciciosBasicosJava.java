55f1f25a9e8b04bd670bd1a7e4febf659936fce1
xp <xp@CETYSDAM00>
2019-11-28 15:57:54 +0100

    Con el segundo ejercicio empezado

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -18,6 +18,18 @@ public class EjerciciosBasicosJava {
             return false;
         }
     
+   
+    
+    public int multa (int velocidad, boolean birthday){
+        
+       
+        return 0;
+        }
+    
+     
+        
+    
+    
     /**
      * @param args the command line arguments
      */
